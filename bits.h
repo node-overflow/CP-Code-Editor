a4c0c8d79ecd5c82f59423d7a561a8a52028b7be
Piyush Jha <piyushjha.exe@gmail.com>
2025-10-03 13:23:40 +0530

    header speed optimization

---
+++ bits.h
@@ -0,0 +1,18 @@
+#ifndef BITS_H
+#define BITS_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+#endif
--- temp.cpp
+++ temp.cpp
@@ -1,11 +1,18 @@
-#include <bits/stdc++.h>
+#include "bits.h"
+
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+vector<int> v;
+for (int i = 0; i < 5; i++) v.push_back(i);
+for (int i = 0; i < 5; i++) cout << v[i] << " ";
+cout << endl;
     int n;
     cin >> n;
     cout << n << "\n";
+
     return 0;
 }
